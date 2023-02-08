13acef637d1acf13ede65175357a140fc5801aa2
ashraful005 <alam1809005@stud.kuet.ac.bd>
2023-02-08 22:32:13 +0600

    concatanation

--- main.go
+++ main.go
@@ -4,4 +4,9 @@ import "fmt"
 
 func main(){
 fmt.Println("1 + 1 = ",1.0+1.0)
+fmt.Println("Hello World")
+fmt.Println(len("Hello World"))
+fmt.Println("Hello World"[1])
+fmt.Println("Hello " + "World")
+
 }
