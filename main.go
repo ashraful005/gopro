3509f0643860448328ea0477914a0457fb5a5f1e
ashraful005 <alam1809005@stud.kuet.ac.bd>
2023-02-10 22:20:17 +0600

    final

--- main.go
+++ main.go
@@ -3,7 +3,11 @@ package main
 import "fmt"
 
 func main(){
-fmt.Println("1 + 1 = ",1.0+1.0)
+//both gives same output
+fmt.Println("1 + 1 = ",1+1)      //integer
+fmt.Println("1 + 1 = ",1.0+1.0)  //floating number
+
+//strings
 fmt.Println("Hello World")
 fmt.Println(len("Hello World"))
 fmt.Println("Hello World"[1])
@@ -12,6 +16,8 @@ fmt.Println("Hello " + "World")
 fmt.Println("Hello" == "World")
 fmt.Println("Hello" == "Hello")
 
+
+//variables vs constants
 var a int = 22
 const b int =22
 fmt.Println(a)
